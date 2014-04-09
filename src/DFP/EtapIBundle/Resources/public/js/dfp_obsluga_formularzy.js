6cc27171e8cf07870da6b422fc5824226fe4603d
Goldi-PC-Ubuntu <adamkrolikowski@vp.pl>
2014-04-09 20:47:42 +0200

    Dodanie kompletnej funkcji ajax pobierającej opis procesu aplikacji przy zmianie wartosci pola select.

--- src/DFP/EtapIBundle/Resources/public/js/dfp_obsluga_formularzy.js
+++ src/DFP/EtapIBundle/Resources/public/js/dfp_obsluga_formularzy.js
@@ -62,14 +62,80 @@
 
     }
 
-    $.fn.qTipHelper = function()
+    $.fn.qTipHelper = function(options)
     {
         var $this = $(this);
 
-        $this.qtip({
-            content: {
-                text: 'test'
+        var settings = $.extend({
+            ajax: false,
+            przypisz_do: '',
+            url: ''
+        },options);
+
+        var qTipOptionsDefaults = {
+            style: {
+                widget: true,
+                def: false
+            },
+            position: {
+                my: 'bottom right',
+                at: 'top center',
+                adjust: {
+                    y: -10
+                }
+            },
+            show: {
+                event: 'click mouseover',
+                solo: true
+            },
+            hide: {
+                event: 'unfocus'
             }
-        });
+        };
+
+        if(settings.ajax == false)
+        {
+            $this.qtip(qTipOptionsDefaults);
+        }else{
+            var qTipOptions = $.extend({
+                overwrite: false, // Make sure the tooltip won't be overridden once created
+                content: 'Wybierz parametr!',
+                show: {
+                    ready: true // Show the tooltip as soon as it's bound, vital so it shows up the first time you hover!
+                }
+            }, qTipOptionsDefaults);
+
+            var $helperContainer = $this.find('.'+settings.przypisz_do);
+            $helperContainer.on('mouseover','select', function(event){
+                $(this).qtip(qTipOptions, event);
+            })
+            .each(function(i) {
+                $.attr(this, 'oldtitle', $.attr(this, 'title'));
+                this.removeAttribute('title');
+            });
+
+            $helperContainer.on('change','select',function(){
+                var idParametru = this.value;
+                $(this).qtip('api').set('content.text', function(event, api){
+                    $.get(settings.url,
+                    {
+                        id: idParametru
+                    },
+                    function(response)
+                    {
+                        if(response.code == 100 && response.success)
+                        {
+                            if(response.opis != null)
+                            {
+                                api.set('content.text', response.opis)
+                            }
+                        }
+                    }
+                    );
+                });
+            })
+
+        }
+
     }
 })(jQuery);
