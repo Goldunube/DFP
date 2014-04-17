c0c17536e34e26ab11e903cd94c2fea5e3f2210b
Goldi-PC-Ubuntu <adamkrolikowski@vp.pl>
2014-04-17 20:30:12 +0200

    Dodanie tablicy z kategoriami notatek do showAction w kontrolerze Fila po stronie backendu.

--- src/DFP/EtapIBundle/Controller/Backend/FilieController.php
+++ src/DFP/EtapIBundle/Controller/Backend/FilieController.php
@@ -54,8 +54,17 @@ class FilieController extends Controller
         if (!$filia) {
             throw $this->createNotFoundException('Nie znaleziono karty klienta.');
         }
+
+        $kategorieNotatek = array(
+            1 => 'Wymagania klienta',
+            2 => 'Informacje handlowe',
+            3 => 'Harmonogram działań',
+            4 => 'Notatki z wizyt'
+        );
+
         return array(
             'filia' => $filia,
+            'notatka_kategorie' => $kategorieNotatek
         );
     }
 
