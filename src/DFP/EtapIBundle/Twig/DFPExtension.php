60dbab9675633c5869594f2764674a410ae6d865
Goldi-PC-Ubuntu <adamkrolikowski@vp.pl>
2014-04-27 07:57:04 +0200

    Zmiana formatu wygladu filtra numerów NIP na 000 00 00 000

--- src/DFP/EtapIBundle/Twig/DFPExtension.php
+++ src/DFP/EtapIBundle/Twig/DFPExtension.php
@@ -19,7 +19,7 @@ class DFPExtension extends \Twig_Extension
 
     public function nipFilter($numer)
     {
-        $nip = sprintf("%s-%s-%s-%s",substr($numer,0,3),substr($numer,3,2),substr($numer,5,2),substr($numer,7));
+        $nip = sprintf("%s %s %s %s",substr($numer,0,3),substr($numer,3,2),substr($numer,5,2),substr($numer,7));
 
         return $nip;
     }
