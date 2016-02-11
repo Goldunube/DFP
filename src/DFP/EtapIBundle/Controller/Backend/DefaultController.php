819fee25192e340b82a8863b77459123eb304b29
Adam <adamkrolikowski@vp.pl>
2016-02-11 15:58:14 +0100

    Zmiana sposobu wyświetlania listy słowników

--- src/DFP/EtapIBundle/Controller/Backend/DefaultController.php
+++ src/DFP/EtapIBundle/Controller/Backend/DefaultController.php
@@ -23,6 +23,26 @@ class DefaultController extends Controller
      */
     public function listaSlownikowAction()
     {
-        return array();
+        $slowniki = array(
+            'Grupy klientów'            =>  $this->generateUrl('grupaklientow'),
+            'Grupy produktów'           =>  $this->generateUrl('grupaproduktow'),
+            'Kolory'                    =>  $this->generateUrl('backend_kolor'),
+            'Materiały uzupełniające'   =>  $this->generateUrl('url_materialuzupelniajacy'),
+            'Metody aplikacji farby'    =>  $this->generateUrl('url_procesaplikacji'),
+            'Metody przygotowania powierzchni'  =>  $this->generateUrl('url_procesprzygotowaniapowierzchni'),
+            'Parametry produktu'        =>  $this->generateUrl('url_wymaganiaproduktu'),
+            'Parametry utwardzania powłoki'     =>  $this->generateUrl('url_procesutwardzaniapowloki'),
+            'Profile działalności'      =>  $this->generateUrl('url_profiledzialalnosci'),
+            'Rodzaje podłoża'           =>  $this->generateUrl('url_backend_rodzajpowierzchni'),
+            'Wymagania powłoki lakierowej'      =>  $this->generateUrl('url_wymaganiapowloki'),
+            'Wzorniki kolorów'          =>  $this->generateUrl('backend_wzornikkolorow'),
+            'Rodzaje wizyt technicznych'=>  $this->generateUrl('backend_rodzaje_zdarzen_technicznych')
+        );
+
+        ksort($slowniki);
+
+        return array(
+            'slowniki'  =>  $slowniki
+        );
     }
 }
